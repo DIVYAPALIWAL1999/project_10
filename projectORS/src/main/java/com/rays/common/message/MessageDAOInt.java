3589bc0196ab92d9d209fd866328ac2db9a62e42
Mohit <mohitpaliwal90@gmail.com>
2023-10-11 12:46:50 +0530

    succ

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Divya Paliwal
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Divya Paliwal
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
